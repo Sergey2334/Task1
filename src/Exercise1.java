07f74b4a0139fefc0098d16c6ab3a59edd4e7dae
sergh <sergh@edu.aac.ac.il>
2025-11-23 16:47:37 +0200

    Task 1 First Commit Exercise 1: Quadratic Formula Notes

---
+++ src/Exercise1.java
@@ -0,0 +1,28 @@
+public class Exercise1 {
+    public static void main(String[] args) {
+        /*
+        =============================================================================================
+        Program that calculates a quadratic formula:
+        (a * x^2) +- (b * x) +- (c) = 0
+        a, b, c
+        delta = (b ^ 2) - (4 * a * c)
+        x1,2 = (-b +- sqrt(delta)) / (2 * a)
+
+        Example:
+        x^2 + 3x - 4 = 0
+        a = 1, b = 3, c = -1
+        delta = (3 ^ 2) - (4 * 1 * (-4)) = 9 - (-16) = 25
+        x1 = ((-3) + sqrt(delta)) / (2 * 1) , (+) = (-3 + sqrt(25)) / (2) = (-3 + 5) / (2) = 1
+        x2 = ((-3) - sqrt(delta)) / (2 * 1) , (-) = (-3 - sqrt(25)) / (2) = (-3 - 5) / (2) = -4
+
+        can be 2 answers x1,2 , delta > 0
+        can be 1 answer , delta = 0
+        can be 0 answer* , delta < 0
+        (* In that case delta is "i" and the answer is a Complex number "z = a + bi")
+        =============================================================================================
+        */
+
+
+
+    }
+}
